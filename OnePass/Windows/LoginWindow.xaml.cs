2e6a41694848b5040169471eee145e817a2f4277
Callum Anning <CallumAnning@hotmail.co.uk>
2021-02-01 17:59:12 +0000

    Added validation messages to login window

--- OnePass/Windows/LoginWindow.xaml.cs
+++ OnePass/Windows/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnePass.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,68 @@ namespace OnePass.Windows
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnClick_Login(object sender, RoutedEventArgs e)
+        {
+            var usernameValid = ValidateUsernameSyntax();
+            var passwordValid = ValidatePasswordSyntax();
+
+            if (usernameValid && passwordValid)
+            {
+                MessageBox.Show("Verify");
+            }
+        }
+
+        private void OnTextChanged_ValidateSyntax(object sender, TextChangedEventArgs e)
+        {
+            ValidateUsernameSyntax();
+        }
+
+        private bool ValidateUsernameSyntax()
+        {
+            var username = Username.Text;
+
+            UsernameValidationMessage.Visibility = Visibility.Visible;
+            if (string.IsNullOrEmpty(username))
+            {
+                UsernameValidationMessage.Content = "Username is required.";
+                return false;
+            }
+            else
+            {
+                UsernameValidationMessage.Visibility = Visibility.Collapsed;
+                UsernameValidationMessage.Content = string.Empty;
+                return true;
+            }
+        }
+
+        private void OnPasswordChanged_ValidateSyntax(object sender, RoutedEventArgs e)
+        {
+            ValidatePasswordSyntax();
+        }
+
+        private bool ValidatePasswordSyntax()
+        {
+            var password = Password.Password;
+
+            PasswordValidationMessage.Visibility = Visibility.Visible;
+            if (string.IsNullOrEmpty(password))
+            {
+                PasswordValidationMessage.Content = "Password is required.";
+                return false;
+            }
+            else
+            {
+                PasswordValidationMessage.Visibility = Visibility.Collapsed;
+                PasswordValidationMessage.Content = string.Empty;
+                return true;
+            }
         }
     }
 }
