89e66ef15087dd6fd2e108923b3ce20f2502c293
Callum Anning <CallumAnning@hotmail.co.uk>
2021-07-06 00:42:17 +0100

    Added android project

---
+++ OnePass.Android/LoginActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace OnePass.Android
+{
+    [Activity(Label = "OnePass", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class LoginActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_login);
+
+            // Create your application here
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            var versionLabel = FindViewById<TextView>(Resource.Id.login_version);
+            versionLabel.Text = $"v{version.ToString(3)}";
+        }
+    }
+}
