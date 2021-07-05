8948c54218f64cfedda5e55be09d0e4fea9839f9
Callum Anning <CallumAnning@hotmail.co.uk>
2021-07-06 00:42:17 +0100

    Added android project

---
+++ OnePass.Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Android.Content.PM;
+
+namespace OnePass.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+	}
+}
