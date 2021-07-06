1973d088e6e97dcb6bfe4b42307a410e838727a1
Callum Anning <CallumAnning@hotmail.co.uk>
2021-07-06 21:42:11 +0100

    Added floating action button

--- OnePass.Android/MainActivity.cs
+++ OnePass.Android/MainActivity.cs
@@ -19,6 +19,23 @@ namespace OnePass.Android
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            // var username = Intent.GetStringExtra("Username")
+
+            // Add floating action button
+            var addFab = FindViewById<FloatingActionButton>(Resource.Id.add_fab);
+            addFab.Click += AddFab_Click;
         }
-	}
+
+        private void AddFab_Click(object sender, EventArgs e)
+        {
+            var dialog = new AndroidX.AppCompat.App.AlertDialog.Builder(this);
+            dialog.SetTitle("Alert");
+            dialog.SetMessage("message");
+            dialog.SetPositiveButton("OK", (sender, args) => { });
+
+            var alert = dialog.Create();
+            alert.Show();
+        }
+    }
 }
