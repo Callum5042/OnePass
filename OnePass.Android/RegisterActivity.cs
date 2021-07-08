5b9f6b769fdfdce1f8d3c54bdc9d2573d3818085
Callum Anning <CallumAnning@hotmail.co.uk>
2021-07-08 18:22:13 +0100

    Added register activity

---
+++ OnePass.Android/RegisterActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnePass.Android
+{
+    [Activity(Theme = "@style/AppTheme")]
+    public class RegisterActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_register);
+        }
+    }
+}
