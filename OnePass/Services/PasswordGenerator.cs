29c4c11bf1838b59e7b9250996dc0e50ba914f86
Callum Anning <CallumAnning@hotmail.co.uk>
2021-07-08 13:42:15 +0100

    Added edit account page

---
+++ OnePass/Services/PasswordGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnePass.Services
+{
+    public class PasswordGenerator1
+    {
+        public string Generate()
+        {
+            return "Generated";
+        }
+    }
+}
