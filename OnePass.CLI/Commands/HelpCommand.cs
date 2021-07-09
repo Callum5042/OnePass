3b3cdb3bb67487efb2fe0df6b8a0701c17ec7aeb
Callum Anning <CallumAnning@hotmail.co.uk>
2021-07-09 16:03:45 +0100

    Fixed up OnePass.CLI not passing passwords

--- OnePass.CLI/Commands/HelpCommand.cs
+++ OnePass.CLI/Commands/HelpCommand.cs
@@ -10,6 +10,12 @@ namespace OnePass.CLI.Commands
         public Task ExecuteAsync(Arguments arguments)
         {
             Console.WriteLine("=== OnePass CLI - Help ===");
+            Console.WriteLine("-[encrypt/decrypt] -file \"filename.bin\" -password \"pass\"");
+
+            Console.WriteLine("= Encryprt Example =");
+            Console.WriteLine("-decrypt -file \"filename.bin\" -password \"password\"");
+            Console.WriteLine("= Decrypt Example =");
+            Console.WriteLine("-encrypt -file \"filename.bin\" -password \"password\"");
 
             return Task.CompletedTask;
         }
