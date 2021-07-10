763da383e5f73cd94b68711381e8b213fd5c73a7
Callum Anning <CallumAnning@hotmail.co.uk>
2021-07-10 12:14:55 +0100

    Added validation to register activity

--- OnePass.Android/Activities/RegisterActivity.cs
+++ OnePass.Android/Activities/RegisterActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Views;
 using Android.Widget;
 using OnePass.Models;
 using OnePass.Services;
@@ -18,6 +19,10 @@ namespace OnePass.Droid.Activities
         private EditText _password;
         private EditText _repeatPassword;
 
+        private TextView _usernameTextView;
+        private TextView _passwordTextView;
+        private TextView _passwordRepeatTextView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -29,6 +34,10 @@ namespace OnePass.Droid.Activities
             _password = FindViewById<EditText>(Resource.Id.login_password);
             _repeatPassword = FindViewById<EditText>(Resource.Id.login_password_repeat);
 
+            _usernameTextView = FindViewById<TextView>(Resource.Id.login_username_validation_message);
+            _passwordTextView = FindViewById<TextView>(Resource.Id.login_password_validation_message);
+            _passwordRepeatTextView = FindViewById<TextView>(Resource.Id.login_password_repeat_validation_message);
+
             // Button
             var registerButton = FindViewById<Button>(Resource.Id.register_button);
             registerButton.Click += RegisterButton_Click;
@@ -38,10 +47,25 @@ namespace OnePass.Droid.Activities
         {
             var encryptor = new FileEncryptor();
 
+            var isValid = Validate();
+            if (!isValid)
+            {
+                return;
+            }
+
             // Create initial encrypted file
             var documentsPath = GetExternalFilesDir(Android.OS.Environment.DirectoryDocuments).AbsolutePath;
             var filename = $"{_username.Text}.bin";
             var path = Path.Combine(documentsPath, filename);
+
+            if (File.Exists(path))
+            {
+                _usernameTextView.Text = "Username already exists";
+                _usernameTextView.Visibility = ViewStates.Visible;
+
+                Toast.MakeText(this, "Username already exists", ToastLength.Short).Show();
+                return;
+            }
 
             var json = JsonSerializer.Serialize(new List<Account>());
             var buffer = Encoding.UTF8.GetBytes(json);
@@ -52,6 +76,44 @@ namespace OnePass.Droid.Activities
 
             // Finish
             Finish();
+        }
+
+        private bool Validate()
+        {
+            _usernameTextView.Visibility = ViewStates.Gone;
+            _passwordTextView.Visibility = ViewStates.Gone;
+            _passwordRepeatTextView.Visibility = ViewStates.Gone;
+
+            var isValid = true;
+            if (string.IsNullOrEmpty(_username.Text))
+            {
+                isValid = false;
+                _usernameTextView.Text = "Username is required";
+                _usernameTextView.Visibility = ViewStates.Visible;
+            }
+
+            if (string.IsNullOrEmpty(_password.Text))
+            {
+                isValid = false;
+                _passwordTextView.Text = "Password is required";
+                _passwordTextView.Visibility = ViewStates.Visible;
+            }
+
+            if (string.IsNullOrEmpty(_repeatPassword.Text))
+            {
+                isValid = false;
+                _passwordRepeatTextView.Text = "Repeat Password is required";
+                _passwordRepeatTextView.Visibility = ViewStates.Visible;
+            }
+
+            if (_password.Text != _repeatPassword.Text)
+            {
+                isValid = false;
+                _passwordRepeatTextView.Text = "Passwords do not match";
+                _passwordRepeatTextView.Visibility = ViewStates.Visible;
+            }
+
+            return isValid;
         }
     }
 }
