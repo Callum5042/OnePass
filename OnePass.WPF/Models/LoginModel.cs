abbd2252a5f7e5ebafdf33883d3285d2480dc166
Callum Anning <CallumAnning@hotmail.co.uk>
2022-04-09 17:41:13 +0100

    Added validation

--- OnePass.WPF/Models/LoginModel.cs
+++ OnePass.WPF/Models/LoginModel.cs
@@ -2,6 +2,8 @@
 using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -11,12 +13,29 @@ using System.Windows.Input;
 
 namespace OnePass.WPF.Models
 {
-    public class LoginModel : ObservableObject
+    public class LoginModel : ObservableValidator
     {
         public LoginModel()
         {
             LoginCommand = new RelayCommand(Login);
             RegisterCommand = new RelayCommand(Register);
+
+            ErrorsChanged += LoginModel_ErrorsChanged;
+        }
+
+        private void LoginModel_ErrorsChanged(object sender, DataErrorsChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Username))
+            {
+                UsernameValidationMessage = GetErrors(e.PropertyName).Select(x => x.ErrorMessage).FirstOrDefault();
+                UsernameLabelVisibility = GetErrors(e.PropertyName).Any() ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            if (e.PropertyName == nameof(Password))
+            {
+                PasswordValidationMessage = GetErrors(e.PropertyName).Select(x => x.ErrorMessage).FirstOrDefault();
+                PasswordLabelVisibility = GetErrors(e.PropertyName).Any() ? Visibility.Visible : Visibility.Collapsed;
+            }
         }
 
         public static string Version => $"v{Assembly.GetExecutingAssembly().GetName().Version.ToString(3)}";
@@ -27,7 +46,7 @@ namespace OnePass.WPF.Models
 
         private void Login()
         {
-            MessageBox.Show("Login");
+            ValidateAllProperties();
         }
 
         private void Register()
@@ -35,8 +54,55 @@ namespace OnePass.WPF.Models
 
         }
 
-        public string Username { get; set; }
+        private string _username;
 
-        public string Password { get; set; }
+        [Required]
+        public string Username 
+        {
+            get => _username;
+            set => SetProperty(ref _username, value, validate: true);
+        }
+
+        private string _password;
+
+        [Required]
+        [MinLength(10, ErrorMessage = $"{nameof(Password)} must be at least 10 characters")]
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value, validate: true);
+        }
+
+        private string _usernameValidationMessage;
+
+        public string UsernameValidationMessage 
+        {
+            get => _usernameValidationMessage;
+            set => SetProperty(ref _usernameValidationMessage, value);
+        }
+
+        private Visibility _visibilityUsernameLabel = Visibility.Collapsed;
+
+        public Visibility UsernameLabelVisibility 
+        {
+            get => _visibilityUsernameLabel;
+            set => SetProperty(ref _visibilityUsernameLabel, value);
+        }
+
+        private string _passwordValidationMessage;
+
+        public string PasswordValidationMessage
+        {
+            get => _passwordValidationMessage;
+            set => SetProperty(ref _passwordValidationMessage, value);
+        }
+
+        private Visibility _visibilitypasswordLabel = Visibility.Collapsed;
+
+        public Visibility PasswordLabelVisibility
+        {
+            get => _visibilitypasswordLabel;
+            set => SetProperty(ref _visibilitypasswordLabel, value);
+        }
     }
 }
