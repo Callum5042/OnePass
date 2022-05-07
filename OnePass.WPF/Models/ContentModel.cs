a0fa56b708aea5a87cb0e0b5fb2b33e0cef1a6a6
Callum Anning <CallumAnning@hotmail.co.uk>
2022-05-07 11:48:36 +0100

    Added filtering list

--- OnePass.WPF/Models/ContentModel.cs
+++ OnePass.WPF/Models/ContentModel.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using OnePass.Infrastructure;
 using OnePass.WPF.Services;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 
 namespace OnePass.WPF.Models
 {
@@ -10,13 +13,14 @@ namespace OnePass.WPF.Models
     public class ContentModel : ObservableObject
     {
         private readonly FileEncoder _fileEncoder;
+        private readonly IEnumerable<AccountListModel> _accountListModel;
 
         public ContentModel()
         {
             _fileEncoder = new FileEncoder();
             _fileEncoder.Load();
 
-            Accounts = new ObservableCollection<AccountListModel>(_fileEncoder.Accounts.OrderBy(x => x.Name).Select(x => new AccountListModel()
+            _accountListModel = _fileEncoder.Accounts.OrderBy(x => x.Name).Select(x => new AccountListModel()
             {
                 Guid = x.Guid,
                 Name = x.Name,
@@ -24,10 +28,13 @@ namespace OnePass.WPF.Models
                 EmailAddress = x.EmailAddress,
                 Password = x.Password,
                 DateModified = x.DateModified,
-            }));
+            });
+
+            Accounts = new ObservableCollection<AccountListModel>(_accountListModel);
         }
 
-        public ObservableCollection<AccountListModel> Accounts { get; private set; } = new ObservableCollection<AccountListModel>();
+        public ObservableCollection<AccountListModel> Accounts { get => accounts; private set => SetProperty(ref accounts, value); }
+        private ObservableCollection<AccountListModel> accounts = new ObservableCollection<AccountListModel>();
 
         public void Remove(AccountListModel model)
         {
@@ -40,5 +47,45 @@ namespace OnePass.WPF.Models
             // Remove from view
             Accounts.Remove(model);
         }
+
+        public string Search
+        {
+            get => search;
+            set
+            {
+                SetProperty(ref search, value);
+
+                // Filter list
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Accounts = new ObservableCollection<AccountListModel>(_accountListModel);
+                }
+                else
+                {
+                    var filter = _accountListModel.Where(x =>
+                    { 
+                        if (x.Name?.Contains(value, StringComparison.CurrentCultureIgnoreCase) == true)
+                        {
+                            return true;
+                        }
+
+                        if (x.Username?.Contains(value, StringComparison.CurrentCultureIgnoreCase) == true)
+                        {
+                            return true;
+                        }
+
+                        if (x.EmailAddress?.Contains(value, StringComparison.CurrentCultureIgnoreCase) == true)
+                        {
+                            return true;
+                        }
+
+                        return false;
+                    });
+
+                    Accounts = new ObservableCollection<AccountListModel>(filter);
+                }
+            }
+        }
+        private string search;
     }
 }
