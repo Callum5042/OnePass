85ffb79598324d20f12f5551d23f882a174b8d60
Callum Anning <CallumAnning@hotmail.co.uk>
2022-06-26 21:41:44 +0100

    Current style

---
+++ OnePass.Web/Site/PasswordGenerator/PasswordGeneratorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnePass.Web.Site.PasswordGenerator
+{
+    public class PasswordGeneratorController : Controller
+    {
+        [Route("generate-password")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
